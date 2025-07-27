fa79b8234d078396a1b5dd29c9568943a68c98f4
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-07-27 10:46:35 +0330

    add resource validations and data to lessoncategory

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Lesson;
 use App\Http\Requests\StoreLessonRequest;
 use App\Http\Requests\UpdateLessonRequest;
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\LessonResource;
+use App\Models\Category;
 
 class LessonController extends Controller
 {
@@ -13,54 +16,45 @@ class LessonController extends Controller
      */
     public function index()
     {
-        //
+        return LessonResource::collection(Lesson::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
+  
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreLessonRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+        return new CategoryResource($category);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Lesson $lesson)
+    public function show(Lesson $lesson ,$id)
     {
-        //
+        return new CategoryResource(Category::findOrFail($id));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Lesson $lesson)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateLessonRequest $request, Lesson $lesson)
+    public function update(UpdateLessonRequest $request, Lesson $lesson ,$id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->update($request->validated());
+        return new CategoryResource($category);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Lesson $lesson)
+    public function destroy($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json(['message' => 'Lesson Deleted successfully']);
     }
 }
