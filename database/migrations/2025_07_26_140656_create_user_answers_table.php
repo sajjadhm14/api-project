0e428fef5698271d7a109405d9f269b8ee4f8332
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-07-27 10:13:56 +0330

    Update migrations: modified tables and removed ticket table

--- database/migrations/2025_07_26_140656_create_user_answers_table.php
+++ database/migrations/2025_07_26_140656_create_user_answers_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user__answers');
+        Schema::dropIfExists('user_answers');
     }
 };
--- database/migrations/2025_07_26_141054_create_user_lessons_table.php
+++ database/migrations/2025_07_26_141054_create_user_lessons_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user__lessons');
+        Schema::dropIfExists('user_lessons');
     }
 };
