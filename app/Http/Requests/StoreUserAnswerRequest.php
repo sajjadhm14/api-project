3195850eaba5acb6432b744354f33c5f3dec2ac2
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-07-31 21:09:22 +0330

    make auth check in store user request

--- app/Http/Requests/StoreUserAnswerRequest.php
+++ app/Http/Requests/StoreUserAnswerRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreUserAnswerRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreUserAnswerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
