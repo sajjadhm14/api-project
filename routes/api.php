cb0fb0b705bb9975eaaac0fd066cc1c85d8b6dea
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-08-03 15:09:58 +0330

    update  routes

--- routes/api.php
+++ routes/api.php
@@ -26,49 +26,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('category',CategoryController::class);
     Route::apiResource('lesson',LessonController::class);
+    Route::apiResource('question',QuestionController::class);
+    Route::apiResource('textanswer',TextAnswerController::class);
+    Route::apiResource('selectoption', SelectOptionController::class);
+    Route::apiResource('useranswers',UserAnswerController::class);
+    Route::apiResource('userlessons',UserLessonsController::class);
+
 });
 
-
-
-// Route::middleware('auth:sanctum')->prefix('category')->group(function(){
-//     Route::get('',[CategoryController::class , 'index']);
-//     Route::get('{category}',[CategoryController::class, 'show']);
-//     Route::post('',[CategoryController::class, 'store']);
-//     Route::put('{category}',[CategoryController::class, 'update']);
-//     Route::delete('{category}',[CategoryController::class, 'destroy']);
-// });
-
-Route::middleware('auth:sanctum')->prefix('lesson')->group(function(){
-    Route::get('',[LessonController::class , 'index']);
-    Route::post('start',[LessonController::class , 'startlessonwithquestion']);
-    Route::get('{lesson}',[LessonController::class, 'show']);
-    Route::post('',[LessonController::class, 'store']);
-    Route::put('{lesson}',[LessonController::class, 'update']);
-    Route::delete('{lesson}',[LessonController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->prefix('question')->group(function(){
-    Route::get('',[QuestionController::class , 'index']);
-    Route::get('{question}',[QuestionController::class, 'show']);
-    Route::post('',[QuestionController::class, 'store']);
-    Route::put('{question}',[QuestionController::class, 'update']);
-    Route::delete('{question}',[QuestionController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->prefix('textanswer')->group(function(){
-    Route::get('',[TextAnswerController::class , 'index']);
-    Route::get('{textanswer}',[TextAnswerController::class, 'show']);
-    Route::post('',[TextAnswerController::class, 'store']);
-    Route::put('{textanswer}',[TextAnswerController::class, 'update']);
-    Route::delete('{textanswer}',[TextAnswerController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->prefix('selectoption')->group(function(){
-    Route::get('',[SelectOptionController::class , 'index']);
-    Route::get('{selectoption}',[SelectOptionController::class, 'show']);
-    Route::post('',[SelectOptionController::class, 'store']);
-    Route::put('{selectoption}',[SelectOptionController::class, 'update']);
-    Route::delete('{selectoption}',[SelectOptionController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->apiResource('useranswers',UserAnswerController::class);
-Route::middleware('auth:sanctum')->apiResource('userlessons',UserLessonsController::class);
 
 
 Route::post('/login', [AuthController::class, 'login']);
