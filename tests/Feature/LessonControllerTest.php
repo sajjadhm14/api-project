899423015d76414571d7f045773a8abd50632f8b
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-08-05 11:06:29 +0330

    make test with phpunit for LessonController

---
+++ tests/Feature/LessonControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Lesson;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LessonControllerTest extends TestCase
+{
+
+    use RefreshDatabase;
+   public function test_it_can_list_lessons()
+   {
+        Lesson::factory()->count(3)->create();
+
+        $response = $this->getJson('api/lesson');
+
+        $response -> assertStatus(200)
+                  -> assertJsonCount(3,'data');
+
+   }
+
+   public function test_it_can_create_lessons()
+   {
+        $data = ['name' => 'lessonTest'];
+
+        $response = $this->postJson('api/lesson', $data);
+
+        $response ->assertStatus(201)
+                  ->assertJsonPath('data.name', 'lessonTest');
+
+        $this -> assertDatabaseHas('lessons',['name'=> 'lessonTest']);
+   }
+
+   public function test_it_can_update_lessons()
+   {
+        $lesson = Lesson::factory()->create();
+
+        $response = $this->putJson('/api/lesson/{$lesson->id}',['name' => 'updateLesson']);
+
+        $response -> assertStatus(200)
+                  ->assertJsonPath('data.name', 'updateLesson');
+        $this-> assertDatabaseHas('lessons', ['name' => 'updateLesson']);
+
+   }
+
+
+   public function test_it_can_delete_lessons()
+   {
+        $lesson = Lesson::factory()->create();
+
+        $response = $this->deleteJson('api/lesson/{$lesson->id}');
+
+        $response -> assertStatus(204);
+
+        $this-> assertDatabaseMissing('lessons',['id'=>$lesson->id]);
+   }
+}
