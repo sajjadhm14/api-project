17b63ed222b9a64b8e7dc3914a37f0c8115a38cb
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-08-05 15:44:05 +0330

    make test with phpUnit for textAnswerController

---
+++ tests/Feature/TextAnswerControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TextAnswer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TextAnswerControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_list_TextAnswer()
+    {
+        TextAnswer :: factory()->count(3)->create();
+
+        $response = $this->getJson('/api/TextAnswer');
+        $response -> assertStatus(200)
+                  ->assertJsonCount(3,'data');
+    }
+
+    public function test_it_can_create_TextAnswer()
+    {
+        $data = ['name'=> 'testTextAnswer'];
+        $response = $this->postJson('/api/TextAnswer',$data);
+
+        $response -> assertStatus(201)
+                  ->assertJsonPath('data.name',['name'=> 'testTextAnswer']);
+        
+        $this-> assertDatabaseHas('textAnswers', ['name'=> 'testTextAnswer']);
+    }
+
+
+    public function test_it_can_update_TextAnswer()
+    {
+        $textAnswer = TextAnswer :: factory()->create();
+        $response = $this->putJson("api/textAnswer/{$textAnswer->id}", ['name'=> 'testUpdate']);
+
+        $response ->assertStatus(200)
+                  ->assertJsonPath('data.name','testUpdate');
+
+        $this->assertDatabaseHas('textAnswers',['name'=>'testUpdate']);
+    }
+
+    public function test_it_can_delete_TextAnswer()
+    {
+        $textAnswer = TextAnswer::factory()->create();
+
+        $response = $this->deleteJson("api/textAnswer/{$textAnswer->id}");
+
+        $this->assertDatabaseMissing('textAnswers',['id'=>$textAnswer->id]);
+    }
+}
