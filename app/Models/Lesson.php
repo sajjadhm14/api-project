641b5df888b12bc950ede4fdc20291a16b549629
sajjadhm14 <sajjadhamidi499@gmail.com>
2025-08-12 11:41:11 +0330

    add StoreBannerRequest

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -4,13 +4,34 @@ namespace App\Models;
 
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Lesson extends Model
+class Lesson extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\LessonFactory> */
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('banner')->singleFile();
+
+        $this->addMediaCollection('images');
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(300)
+            ->height(200)
+            ->nonQueued()
+            ->performOnCollections('banner','images');
+
+        $this->addMediaConversion('webp')
+            ->format('webp')
+            ->performOnCollections('images');
+    }
     protected $fillable = [
         'category_id',
         'title',
